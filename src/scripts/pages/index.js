508c69b63b040217be70aa58941ec4d9fea56709
Hosea174 <hossaena2018@gmail.com>
2024-09-16 12:12:55 +0300

    home hero slider added

---
+++ src/scripts/pages/index.js
@@ -0,0 +1,22 @@
+import Swiper from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade";
+import { Navigation, EffectFade, Autoplay } from "swiper/modules";
+
+const heroBackgrounds = new Swiper(".hero-swiper", {
+  modules: [EffectFade, Navigation, Autoplay],
+  effect: "fade",
+  loop: true,
+  slidesPerView: 1,
+  fadeEffect: {
+    crossFade: false,
+  },
+  autoplay: {
+    delay: 4000,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
