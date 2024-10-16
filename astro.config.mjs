30acc71b7d7e965a03c37a940a38feddc5f599cb
Hosea174 <hossaena2018@gmail.com>
2024-10-16 16:23:20 +0300

    added sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,22 @@ import icon from "astro-icon";
 
 import compressor from "astro-compressor";
 
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), compressor()],
+  site: "https://ahaduglobal.com",
+  integrations: [
+    robotsTxt(),
+    icon(),
+    compressor(),
+    sitemap({
+      filter: (page) =>
+        page !== "https://ahaduglobal.com/terms-and-conditions" &&
+        page !== "https://ahaduglobal.com/privacy-policy",
+    }),
+  ],
   server: {
     host: "0.0.0.0",
     port: 4321,
