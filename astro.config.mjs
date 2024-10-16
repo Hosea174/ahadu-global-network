7ccc0e8b84d072981e8ad89cd4048c14de5686ac
Hosea174 <hossaena2018@gmail.com>
2024-10-16 15:45:37 +0300

    image optimization with astro's Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
+import compressor from "astro-compressor";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [icon(), compressor()],
   server: {
     host: "0.0.0.0",
     port: 4321,
